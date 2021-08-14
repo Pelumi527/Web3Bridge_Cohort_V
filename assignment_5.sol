f19d3a52419b8323567d0b67139e1cfc1677c7cc
Pelumi <fatolupelumi@gmail.com>
2021-08-14 22:27:33 +0100

    assignment_5

---
+++ assignment_5.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract Assignment4{
+    mapping(address => uint) public addresstoffxnint;
+    mapping(address => bytes32) public addressTofxnHash;
+    
+    function fxnone() public returns(bool){
+        addressTofxnHash[msg.sender] = keccak256(abi.encodeWithSignature("fxnone()"));
+        addresstoffxnint[msg.sender]++;
+        return true;
+    }
+    
+    
+     function fxnsecond() public returns(bool){
+         addressTofxnHash[msg.sender] = keccak256(abi.encodeWithSignature("fxnsecond()"));
+         addresstoffxnint[msg.sender]++;
+        return true;
+    }
+    
+     function fxnthird() public returns(bool){
+         addressTofxnHash[msg.sender] = keccak256(abi.encodeWithSignature("fxnthird()"));
+          addresstoffxnint[msg.sender]++;
+        return true;
+    }
+    
+}
